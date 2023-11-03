e9cf594dd68f5849850b04005d083d7c65718c68
hahaclassic <andreyka.steam@yandex.ru>
2023-11-03 16:56:53 +0300

    fix

--- settings.py
+++ settings.py
@@ -1,17 +1,17 @@
 # DATA PATHS
 
-ACCOUNTS_PATH = "./var/lib/tgspam/data/common/accounts"
-LINKS_PATH = "./var/lib/tgspam/data/common/links.txt"
-PHOTOS_PATH = "./var/lib/tgspam/data/photos"
+ACCOUNTS_PATH = "/var/lib/tgspam/data/common/accounts"
+LINKS_PATH = "/var/lib/tgspam/data/common/links.txt"
+PHOTOS_PATH = "/var/lib/tgspam/data/photos"
 
 # BOT SETTINGS
 
-BOT_TOKEN = "5422866414:AAFMCACK2POUWlQFpy16lE9go_K0kEdpM7w"
+BOT_TOKEN = "6613402768:AAFMupXwaIOQSoGcwMoq73PpWGkA15dJ8s0"
 
 # DELAYS
 
-DELAY_BETWEEN_LINKS = 30
+DELAY_BETWEEN_LINKS = 900
 
 # CHATS
 
-ALLOWED_CHATS = [1962810527]
+ALLOWED_CHATS = [1962810527, 1149037793]
